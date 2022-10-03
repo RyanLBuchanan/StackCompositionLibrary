f0106b696e118ca2ca7d9c0bd6c5aff9365c5036
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-10-03 11:01:11 -0600

    Add project files.

---
+++ StackCompositionLibrary/StackComposition.cs
@@ -0,0 +1,40 @@
+﻿using LinkedListLibrary;
+using System.Collections.Generic;
+
+namespace StackCompositionLibrary
+{
+    public class StackComposition
+    {
+        private List stack;
+
+        // Construct empty stack
+        public StackComposition()
+        {
+            stack = new List("stack");
+        }
+
+        // Add object to stack
+        public void Push(object dataValue)
+        {
+            stack.InsertAtFront(dataValue);
+        }
+
+        // Remove object from stack
+        public object Pop()
+        {
+            return stack.RemoveFromFront(); 
+        }
+
+        // Determine whether stack is empty
+        public bool IsEmpty()
+        {
+            return stack.IsEmpty();
+        }
+
+        // Output stack contents
+        public void Display()
+        {
+            stack.Display();
+        }
+    }
+}
